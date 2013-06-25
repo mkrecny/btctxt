4e41b05df13e4d92d12a3ab214a0e1abaaddfa20
mkrecny <mkrecny@gmail.com>
2013-06-25 16:02:58 -0400

    ggpr

--- scrape.js
+++ scrape.js
@@ -1,39 +1,49 @@
 var _ = require('underscore')
 , needle = require('needle')
+, MongoClient = require('mongodb').MongoClient
+, format = require('util').format
 , fs = require('fs')
 , redis = require('redis').createClient()
-, depth_endpoint = 'http://data.mtgox.com/api/2/BTCUSD/MONEY/DEPTH/FETCH';
+, data_endpoint = 'http://data.mtgox.com/api/2/BTCUSD/MONEY/DEPTH/FETCH'
+, expected_writes = 4
+, completed_writes = 0;
 
-var stamp_iterator = function(x){ return x.stamp };
-var stamp_iterator_inv = function(x) { return -x.stamp };
-
-needle.get(depth_endpoint, function(error, response, body){
-
-  if (!body.data){ throw new Error('No data from Mt.Gox :-(')}
-
-  // latest ask & bid
-  var latest_ask = _.max (body.data.asks, stamp_iterator);
-  var latest_bid = _.max (body.data.bids, stamp_iterator); 
-
-  // recent first, old last
-  var asks = _.sortBy(body.data.asks, stamp_iterator_inv);
-  var bids = _.sortBy(body.data.bids, stamp_iterator_inv);
-  
-  var multi = redis.multi();
-
-  asks.forEach(function(ask){
-    multi.sadd('asks', ask.stamp);
-    multi.hmset('ask:'+ask.stamp, ask);
-  });
-
-  bids.forEach(function(bid){
-    multi.sadd('bids', bid.stamp);
-    multi.hmset('bid:'+bid.stamp, bid);
-  });
-
-  multi.exec(function(){
-    console.log(new Date(), 'wrote', bids.length, 'bids', asks.length, 'asks');
+var maybe_end = function(){
+  console.log(arguments);
+  completed_writes+=1;
+  console.log('completed', completed_writes, 'writes');
+  if (completed_writes == expected_writes) {
+    console.log('completed');
     process.exit();
-  });
+  }
+}
 
+var write_to_mongo = function(data, coll){
+  data.forEach(function(d){
+    d._id = d.stamp;
+    console.log(d._id);
+  });
+  MongoClient.connect('mongodb://127.0.0.1:27017/btctxt', function(err, db){
+    var collection = db.collection(coll);
+    collection.update(data, {safe:true, multi:true},  maybe_end)
+  });
+}
+
+var write_to_redis = function(data, key){
+  var multi = redis.multi();
+  data.asks.forEach(function(d){
+    multi.sadd(key, d.stamp);
+    multi.hmset(key+':'+d.stamp, d);
+  });
+  multi.exec(function(){
+    maybe_end();
+  });
+}
+
+needle.get(data_endpoint, function(error, response, body){
+  if (!body.data){ throw new Error('No data'); process.exit();}
+  write_to_redis(body.data.asks, 'asks');
+  write_to_redis(body.data.bids, 'bids');
+  write_to_mongo(body.data.asks, 'asks');
+  write_to_mongo(body.data.bids, 'bids');
 });
